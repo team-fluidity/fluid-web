dc2ab436c29e1001b377e154c9a463aa9bdf407a
kiddieverts <kiddieverts@gmail.com>
2022-03-03 18:04:33 +0000

    Added form validation

--- src/views/MainView/StartStreaming.tsx
+++ src/views/MainView/StartStreaming.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { useWallet } from '@web3-ui/hooks';
 import { ethers } from 'ethers';
 import { HOST_ADDRESS, CFA_ADDRESS, FDAIX_ADDRESS } from 'src/constants';
@@ -37,12 +37,27 @@ type FormProps = { onSubmit: (nftName: string, nftSymbol: string) => void }
 const Form = ({ onSubmit }: FormProps) => {
     const [nftName, setNftName] = useState('');
     const [nftSymbol, setNftSymbol] = useState('');
+    const [nftNameError, setNameError] = useState(false);
+    const [nftSymbolError, setSymbolError] = useState(false);
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        if (!nftName || !nftSymbol) return;
+        let isError = false;
+        if (!nftName) {
+            setNameError(true);
+            isError = true;
+        }
+        if (!nftSymbol) {
+            setSymbolError(true);
+            isError = true;
+        }
+        if (isError) {
+            return;
+        }
         onSubmit(nftName, nftSymbol);
     }
+
+    const handleChangeSymbol = (e: ChangeEvent<HTMLInputElement>) => setNftSymbol(e.target.value.toUpperCase());
 
     return <div className='mt-4 w-3/6 gap-4'>
         <div>
@@ -50,8 +65,8 @@ const Form = ({ onSubmit }: FormProps) => {
         </div>
         <form className='' onSubmit={handleSubmit}>
             <div className='flex gap-4 mt-4'>
-                <input value={nftName} onChange={e => setNftName(e.target.value)} id='nftname' type="text" className='w-3/6 px-3 form-control border border-solid border-gray-300 rounded focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none' placeholder='NFT Name'></input>
-                <input value={nftSymbol} onChange={e => setNftSymbol(e.target.value)} id='nft-symbol' type="text" className='w-3/6 px-3 form-control border border-solid border-gray-300 rounded focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none' placeholder='NFT Symbol'></input>
+                <input style={{ borderColor: nftNameError ? 'red' : undefined }} value={nftName} onChange={e => setNftName(e.target.value)} id='nftname' type="text" className='w-3/6 px-3 form-control border border-solid border-gray-300 rounded focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none' placeholder='NFT Name'></input>
+                <input style={{ borderColor: nftSymbolError ? 'red' : undefined }} maxLength={5} value={nftSymbol} onChange={handleChangeSymbol} id='nft-symbol' type="text" className='w-3/6 px-3 form-control border border-solid border-gray-300 rounded focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none' placeholder='NFT Symbol'></input>
             </div>
             <div className='w-96 flex gap-4'>
                 <button type='submit' className='float-left mt-6 py-2 px-4 rounded-lg bg-gradient-to-r from-orange-500 to-red-500 hover:from-red-500 hover:to-red-700 text-white font-bold'>
